16e886760c3ecd6ae25e9e1428b34253e337446b
Dídac García <didaquis@gmail.com>
2018-05-06 17:11:56 +0200

    Added database

---
+++ assets/database.sql
@@ -0,0 +1,63 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 06-05-2018 a las 17:04:32
+-- Versión del servidor: 10.1.28-MariaDB
+-- Versión de PHP: 7.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `node-scraping-mysql`
+--
+CREATE DATABASE IF NOT EXISTS `node-scraping-mysql` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `node-scraping-mysql`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `scraping_results`
+--
+
+DROP TABLE IF EXISTS `scraping_results`;
+CREATE TABLE `scraping_results` (
+  `id` int(11) NOT NULL,
+  `href` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `results` text COLLATE utf8mb4_unicode_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `scraping_results`
+--
+ALTER TABLE `scraping_results`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `scraping_results`
+--
+ALTER TABLE `scraping_results`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
