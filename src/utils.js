fc1aeaca6f883ed21f7be721bdf747b7e33949e0
Dídac García <didaquis@gmail.com>
2018-07-09 20:34:39 +0200

    Created namespace for docs

--- src/utils.js
+++ src/utils.js
@@ -1,21 +1,32 @@
 /* Home doc */
 /**
  * @file Some useful functions
+ * @see {@link src/utils}
  */
+
+
+/**
+ * @namespace src/utils
+ * @description Some useful functions
+ */
+
 
 /**
  * Remove space, remove non-alphanumeric characters and convert to lower case
  * @param {string} text 
  * @returns {string} Parsed string
+ * @memberof src/utils
  */
 function parseText(text) {
 	return text.replace(/\s+/g, ' ').replace(/[^a-zA-Z0-9 ]/g, ' ').toLowerCase();
 }
 
+
 /**
  * Create an array of words from string.
  * @param {string} text
  * @returns {Array} Array of words 
+ * @memberof src/utils
  */
 function arrayOfWords(text) {
 	const words = [];
